2e5ff7c8b05badaca4395afcb554b0d3bf614922
notnotdee <dlarkinwatson@gmail.com>
2021-07-13 10:33:39 -0700

    04-is-empty test passes

--- objects/exercises/04-is-empty.js
+++ objects/exercises/04-is-empty.js
@@ -7,5 +7,16 @@
  * @returns {boolean}
  */
 export const isEmpty = (object) => {
-  throw new Error(`put your solution here ${object}`);
+  // check the object's keys
+  // if the object has no keys, it is empty
+  if (Object.keys(object).length === 0) return true;
+
+  // this one feels like cheating
+  return !Object.keys(object).filter(
+    (key) => object[key] || object[key] === 0 || object[key] === false
+  ).length;
 };
+
+// should return false, objects has primitive values
+// should return false, shallow check, so [] and {} counts as not empty
+// should return true, objects with null, NaN, undefined, empty string, [], {}
